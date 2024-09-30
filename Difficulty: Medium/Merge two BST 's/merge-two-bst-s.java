98af46b83695d2d140eb23f4b999c5b12cfae48b
Sayantika Laskar <127471376+SayantikaLaskar@users.noreply.github.com>
2024-09-30 09:53:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two BST 's/merge-two-bst-s.java
+++ Difficulty: Medium/Merge two BST 's/merge-two-bst-s.java
@@ -108,52 +108,21 @@ class Node
 
 */
 class Solution {
-    private void DfsPannuBro(Node root, List<Integer> list){
-        if(root == null){
-            return;
-        }
-        DfsPannuBro(root.left, list);
-        list.add(root.data);
-        DfsPannuBro(root.right, list);
-    }
-    
-    private List<Integer> mergePannuBro(List<Integer> list1, List<Integer> list2){
-        List<Integer> res = new ArrayList<>();
-        
-        int n = list1.size(), m = list2.size();
-        int i = 0, j = 0;
-        
-        while(i<n && j<m){
-            int a = list1.get(i), b = list2.get(j);
-            if(a < b){
-                res.add(a);
-                i++;
-            }
-            else{
-                res.add(b);
-                j++;
-            }
-        }
-        
-        while(i<n){
-            res.add(list1.get(i++));
-        }
-        
-        while(j<m){
-            res.add(list2.get(j++));
-        }
-        
-        return res;
-    }
-    
+    // Function to return a list of integers denoting the node
+    // values of both the BST in a sorted order.
     public List<Integer> merge(Node root1, Node root2) {
-        
-        List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
-        
-        DfsPannuBro(root1, list1);
-        DfsPannuBro(root2, list2);
-        
-        return mergePannuBro(list1, list2);
+        // Write your code here
+        List<Integer> sortedList = new ArrayList<>();
+        inOrderTraversal(root1, sortedList);
+        inOrderTraversal(root2, sortedList);
+        sortedList.sort(Integer::compareTo);
+        return sortedList;
+    }
+    
+    private void inOrderTraversal(Node root, List<Integer> sortedList) {
+        if (root == null) return;
+        inOrderTraversal(root.left, sortedList);
+        sortedList.add(root.data);
+        inOrderTraversal(root.right, sortedList);
     }
 }
