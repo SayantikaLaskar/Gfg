b1c27fa7f91839f3fdedaadc4b6443f8735fbacc
Sayantika Laskar <127471376+SayantikaLaskar@users.noreply.github.com>
2025-03-30 11:23:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Gas Station/gas-station.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+
+            ArrayList<Integer> array1 = new ArrayList<Integer>();
+            ArrayList<Integer> array2 = new ArrayList<Integer>();
+
+            String line = read.readLine();
+            String[] tokens = line.split(" ");
+            for (String token : tokens) {
+                array1.add(Integer.parseInt(token));
+            }
+            line = read.readLine();
+            tokens = line.split(" ");
+            for (String token : tokens) {
+                array2.add(Integer.parseInt(token));
+            }
+
+            // ArrayList<Integer> v = new ArrayList<Integer>();
+            int[] gas = new int[array1.size()];
+            int idx = 0;
+            for (int i : array1) gas[idx++] = i;
+
+            int[] cost = new int[array2.size()];
+            idx = 0;
+            for (int i : array2) cost[idx++] = i;
+
+            int ans = new Solution().startStation(gas, cost);
+
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int startStation(int[] gas, int[] cost) {
+        // Your code here
+        int n=gas.length, s=0, st=0, t=0;
+        for(int i=0; i<2*n; i++){
+            int in=i%n;
+            s+=gas[in]-cost[in];
+            t+=gas[in]-cost[in];
+            if(s<0){
+                s=0;
+                st=i+1;
+            }
+        }
+        return t>=0?st%n:-1;
+    }
+}
