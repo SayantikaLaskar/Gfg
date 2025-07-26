1cffc6d42cabd6980f7b80191dd6f3ab29713632
Sayantika Laskar <127471376+SayantikaLaskar@users.noreply.github.com>
2025-07-26 10:47:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element II/majority-element-ii.java
+++ Difficulty: Medium/Majority Element II/majority-element-ii.java
@@ -1,86 +1,18 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        sc.nextLine(); // Consume the newline character
-
-        while (t-- > 0) {
-            String s = sc.nextLine();
-            String[] parts = s.split(" ");
-            int[] nums = new int[parts.length];
-            for (int i = 0; i < parts.length; i++) {
-                nums[i] = Integer.parseInt(parts[i]);
-            }
-            Solution ob = new Solution();
-            List<Integer> ans = ob.findMajority(nums);
-
-            if (ans.size() == 0) {
-                System.out.println("[]");
-            } else {
-                for (int i : ans) {
-                    System.out.print(i + " ");
-                }
-                System.out.println();
-            }
-        }
-        sc.close();
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-    // Function to find the majority elements in the array
-    public List<Integer> findMajority(int[] nums) {
-        // Your code goes here.
-         int n = nums.length;
-        if (n == 0) return new ArrayList<>();
+    public ArrayList<Integer> findMajority(int[] arr) {
+        // Code here
+        int tot=arr.length;
+        ArrayList<Integer> ans = new ArrayList<>();
+        TreeMap<Integer,Integer> mp = new TreeMap<>();
         
-        // Variables to store potential candidates and their counts
-        int candidate1 = 0, candidate2 = 0, count1 = 0, count2 = 0;
-        
-        // Finding potential candidates
-        for (int num : nums) {
-            if (num == candidate1) {
-                count1++;
-            } else if (num == candidate2) {
-                count2++;
-            } else if (count1 == 0) {
-                candidate1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
-                candidate2 = num;
-                count2 = 1;
-            } else {
-                count1--;
-                count2--;
-            }
+        for(int n:arr){
+            mp.put(n,mp.getOrDefault(n,0)+1);
         }
         
-        // Validating candidates
-        count1 = 0;
-        count2 = 0;
-        for (int num : nums) {
-            if (num == candidate1) {
-                count1++;
-            } else if (num == candidate2) {
-                count2++;
-            }
+        for(int k:mp.keySet()){
+            if(tot/3 < mp.get(k)) ans.add(k);
         }
         
-        List<Integer> res = new ArrayList<>();
-        if (count1 > n / 3) {
-            res.add(candidate1);
-        }
-        if (count2 > n / 3) {
-            res.add(candidate2);
-        }
-        Collections.sort(res);
-        return res;
+        return ans;
     }
 }
