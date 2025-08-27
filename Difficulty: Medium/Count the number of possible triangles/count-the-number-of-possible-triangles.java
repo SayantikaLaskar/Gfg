0f3573e51f88644cc40a19515391c6a5139ff09c
Sayantika Laskar <127471376+SayantikaLaskar@users.noreply.github.com>
2025-08-27 18:32:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.java
+++ Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.java
@@ -1,50 +1,21 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Geeks {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                arr[i] = Integer.parseInt(str[i]);
-            }
-            System.out.println(new Solution().countTriangles(arr));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    // Function to count the number of possible triangles.
-    static int countTriangles(int arr[]) {
+    public int countTriangles(int arr[]) {
         // code here
+        int n = arr.length;
         Arrays.sort(arr);
-        int n=arr.length;
-        int c=0;
-        for(int i=2;i<n;i++){
-             int j=0;
-             int k=i-1;
-             while(j < k){
-                   int s=arr[j]+arr[k];
-                   if(s > arr[i]){
-                       c+=k-j;
-                       k--;
-                   }
-                   else{
-                       j++;
-                   }
-             }
+        int count = 0;
+
+        for (int i = 0; i < n - 2; i++) {
+            int k = i + 1;
+            for (int j = i + 2; j < n; j++) {
+                while (k < j && arr[i] + arr[k] <= arr[j]) {
+                    k++;
+                }
+                if (k < j) {
+                    count += (j - k);
+                }
+            }
         }
-        
-        return c;
+        return count;
     }
 }
