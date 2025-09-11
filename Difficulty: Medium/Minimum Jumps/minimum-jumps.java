e30f547c3bf823135072023ea031db8f13a9c133
Sayantika Laskar <127471376+SayantikaLaskar@users.noreply.github.com>
2025-09-11 15:47:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.java
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.java
@@ -1,58 +1,20 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t;
-        t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-
-            System.out.println(new Solution().minJumps(arr));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-    static int minJumps(int[] arr) {
+    public int minJumps(int[] arr) {
         // code here
-        int m=0, j=0, c=0;
-        for(int i=0; i<arr.length; i++){
+        int n=arr.length;
+        if(n==1) return 0;
+        if(arr[0]==0) return -1;
+        int m=arr[0], s=arr[0], j=1;
+        for(int i=1; i<n; i++){
+            if(i==n-1) return j;
             m=Math.max(m, i+arr[i]);
-            if(c==i){
-                if(i!=arr.length-1){
-                    j++;
-                }
-                c=m;
-            }else{
-                continue;
+            s--;
+            if(s==0){
+                j++;
+                if(i>=m) return -1;
+                s=m-i;
             }
         }
-        if(c<arr.length-1){
-            return -1;
-        }
-        return j;
+        return -1;
     }
 }
